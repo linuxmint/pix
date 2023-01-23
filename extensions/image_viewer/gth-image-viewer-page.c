92fec55b9e2f32121787ff1299236c79abcc45b3
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2023-01-23 14:51:56 +0000

    Fix color-profile icon
    It's not a symbolic so it doesn't look right in light themes.

--- extensions/image_viewer/gth-image-viewer-page.c
+++ extensions/image_viewer/gth-image-viewer-page.c
@@ -1170,7 +1170,7 @@ gth_image_viewer_page_real_activate (GthViewerPage *base,
 	self->priv->buttons[APPLY_ICC_PROFILE_BUTTON] =
 			gth_browser_add_header_bar_toggle_button (browser,
 							   	  GTH_BROWSER_HEADER_SECTION_VIEWER_OTHER_COMMANDS,
-								  "color-profile",
+								  "preferences-color-symbolic",
 								  _("Apply the embedded color profile"),
 								  "win.apply-icc-profile",
 								  NULL);
